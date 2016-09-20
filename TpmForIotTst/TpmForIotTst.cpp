98cba73d7fb1efa7d1188eb8db66311d81ab4fb8
Dan Griffin <dan@jwsecure.com>
2016-09-20 13:19:18 -0700

    Refactor server-side calls.

--- TpmForIotTst/TpmForIotTst.cpp
+++ TpmForIotTst/TpmForIotTst.cpp
@@ -8,6 +8,34 @@ using namespace TpmCpp;
 extern void DllInit();
 
 //
+// Open the Endorsement Key
+//
+TPM_HANDLE MakeEndorsementKey(_TPMCPP Tpm2 &tpm)
+{
+    vector<BYTE> NullVec;
+    TPMT_PUBLIC storagePrimaryTemplate(TPM_ALG_ID::SHA1,
+        TPMA_OBJECT::decrypt | TPMA_OBJECT::restricted |
+        TPMA_OBJECT::fixedParent | TPMA_OBJECT::fixedTPM |
+        TPMA_OBJECT::sensitiveDataOrigin | TPMA_OBJECT::userWithAuth,
+        NullVec,           // No policy
+        TPMS_RSA_PARMS(    // How child keys should be protected
+            TPMT_SYM_DEF_OBJECT(TPM_ALG_ID::AES, 128, TPM_ALG_ID::CFB),
+            TPMS_NULL_ASYM_SCHEME(), 2048, 65537),
+        TPM2B_PUBLIC_KEY_RSA(NullVec));
+
+    // Create the key
+    CreatePrimaryResponse ek = tpm.CreatePrimary(
+        tpm._AdminEndorsement,
+        TPMS_SENSITIVE_CREATE(NullVec, NullVec), 
+        storagePrimaryTemplate,
+        NullVec, 
+        vector<TPMS_PCR_SELECTION>());
+
+    return ek.objectHandle;
+}
+
+
+//
 // Open the Storage Root Key
 //
 TPM_HANDLE MakeStoragePrimary(_TPMCPP Tpm2 &tpm)
@@ -152,6 +180,164 @@ void SetPlatformAuthenticationValues(_TPMCPP Tpm2 &tpm)
 }
 
 //
+// Simulated server call that returns a Nonce
+//
+ByteVec ServerGetNonce()
+{
+    ByteVec nonce = CryptoServices::GetRand(16);
+    cout << "Server: Nonce is " << nonce << endl;
+    return nonce;
+}
+
+//
+// Simulate server call that challenges the linkage between an Endorsement Key
+// and another key stored on the same TPM.
+//
+ActivationData ServerGetActivation(
+    TPMT_PUBLIC &clientEkPub, 
+    ByteVec &nameOfKeyToActivate)
+{
+    //
+    // Lookup the client EK public in a whitelist (or, similarly, find its 
+    // certificate and build trust off that). Either way, this is a critical
+    // step, since the EK is the root of trust for the whole protocol, and is 
+    // used to prove that child keys are truly TPM-protected.
+    //
+    // For an example of pulling EK manufacturer certificates from the 
+    // internet, see:
+    // https://github.com/01org/tpm2.0-tools/blob/master/src/tpm2_getmanufec.cpp
+
+    // TODO
+    cout << "Server: EK public is trusted: " << clientEkPub.Serialize(SerializationType::Text) << endl;
+
+    //
+    // Create a random secret and encrypt it back to the client. If the client
+    // can decrypt it, the server can trust that the "activated" key is on the
+    // same TPM as the EK.
+    //
+    // A production server should ideally store each secret, associated with 
+    // the EK pub to which it was encrypted. That way, if the secret is used 
+    // for some downstream purpose (such as a public cloud API key), and gets 
+    // stolen (the secret is exportable; the EK is not), that situation can 
+    // be detected.
+    //
+
+    ByteVec secret = CryptoServices::GetRand(16);
+    cout << "Server: Secret is " << secret << endl;
+    return clientEkPub.CreateActivation(
+        secret,
+        TPM_ALG_ID::SHA1,
+        nameOfKeyToActivate);
+}
+
+void ServerRegisterKey(
+    ByteVec &serverSecret, 
+    TPMT_PUBLIC &clientRestrictedPub,
+    PCR_ReadResponse &clientPcrVals,
+    QuoteResponse &clientPcrQuote, 
+    TPMS_CREATION_DATA &clientKeyCreation,
+    CertifyCreationResponse &clientKeyQuote)
+{
+    //
+    // Confirm that the provided PCR hash is as expected for this particular 
+    // client device. On Windows, an attested boot log can be used instead. On 
+    // Linux, this procedure requires whitelisting.
+    //
+
+    // TODO
+    TPMS_ATTEST qAttest = clientPcrQuote.quoted;
+    TPMS_QUOTE_INFO *qInfo = dynamic_cast<TPMS_QUOTE_INFO *> (qAttest.attested);
+    cout << "Server: quoted PCR is trusted: " << qInfo->pcrSelect[0].ToString() << endl;
+
+    //
+    // Confirm that the client restricted public is the same key that the 
+    // server activated in the previous call
+    //
+
+    // TODO
+    cout << "Server: restricted key is trusted: " << clientRestrictedPub.Serialize(SerializationType::Text) << endl;
+
+    //
+    // Check the PCR quote signature
+    //
+
+    bool sigOk = clientRestrictedPub.ValidateQuote(
+        clientPcrVals, serverSecret, clientPcrQuote);
+    if (sigOk) {
+        cout << "Server: PCR quote is valid" << endl;
+    }
+
+    //
+    // For the new child key, hash the creation data
+    //
+
+    ByteVec creationHash = TPMT_HA::FromHashOfData(
+        TPM_ALG_ID::SHA1, clientKeyCreation.ToBuf()).digest;
+
+    //
+    // Check the parent key identity
+    //
+
+    // TODO
+
+    //
+    // Check the PCR binding
+    //
+
+    // TODO
+
+    // 
+    // Check the key quote signature
+    //
+
+    sigOk = clientRestrictedPub.ValidateCertifyCreation(
+        serverSecret,
+        creationHash,
+        clientKeyQuote);
+    if (sigOk) {
+        cout << "Server: key quote is valid" << endl;
+    }
+}
+
+void ServerReceiveMessage(
+    TPMT_PUBLIC &clientSigningPub,
+    const std::string &clientMessage,
+    TPMU_SIGNATURE &messageSig)
+{
+    //
+    // Confirm that the client signing key is the one registered in the 
+    // previous server call
+    //
+
+    // TODO
+    cout << "Server: client signing key is trusted " << clientSigningPub.Serialize(SerializationType::Text) << endl;
+
+    //
+    // Hash the message
+    //
+
+    ByteVec messageHash = TPMT_HA::FromHashOfString(
+        TPM_ALG_ID::SHA1, clientMessage).digest;
+
+    //
+    // Check the signature
+    //
+
+    bool sigOk = clientSigningPub.ValidateSignature(
+        messageHash, messageSig);
+    if (sigOk) {
+        cout << "Server: message received and verified" << endl;
+    }
+
+    //
+    // Process the message, as appropriate, based on whether the signature
+    // is valid and from a trusted device
+    //
+
+    // TODO
+}
+
+//
 // PCR attestation and AIK activation
 //
 void AttestationForIot()
@@ -191,11 +377,40 @@ void AttestationForIot()
     SetPlatformAuthenticationValues(tpm);
 
     //
+    // Read out the manufacturer Endorsement Key (EK)
+    //
+
+    TPM_HANDLE ekHandle = MakeEndorsementKey(tpm);
+    auto ekPubX = tpm.ReadPublic(ekHandle);
+    TPMT_PUBLIC& ekPub = ekPubX.outPublic;
+
+    //
     // Create a restricted key in the storage hierarchy
     //
 
     TPM_HANDLE primaryKey = MakeStoragePrimary(tpm);
-    TPM_HANDLE signingKey = MakeChildSigningKey(tpm, primaryKey, true);
+    TPM_HANDLE restrictedKey = MakeChildSigningKey(tpm, primaryKey, true);
+    auto restrictedPubX = tpm.ReadPublic(restrictedKey);
+    TPMT_PUBLIC& restrictedPub = restrictedPubX.outPublic;
+
+    //
+    // Request activation to prove linkage between restricted key and EK 
+    //
+
+    ByteVec nameOfKeyToActivate = restrictedKey.GetName();
+    ActivationData encryptedSecret = ServerGetActivation(
+        ekPub, nameOfKeyToActivate);
+
+    //
+    // Activation data can only be decrypted on this TPM
+    //
+
+    ByteVec decryptedSecret = tpm.ActivateCredential(
+        restrictedKey, 
+        ekHandle, 
+        encryptedSecret.CredentialBlob, 
+        encryptedSecret.Secret);
+    cout << "Client: decrypted secret is " << decryptedSecret << endl;
 
     //
     // Optionally, save the AIK, since it can be reused across reboots
@@ -207,38 +422,15 @@ void AttestationForIot()
     // Read PCR data
     //
 
-    cout << ">> PCR Quoting" << endl;
     auto pcrsToQuote = TPMS_PCR_SELECTION::GetSelectionArray(TPM_ALG_ID::SHA1, 7);
     PCR_ReadResponse pcrVals = tpm.PCR_Read(pcrsToQuote);
 
     //
-    // Simulate retrieving a Nonce from the server
-    //
-
-    ByteVec Nonce = CryptoServices::GetRand(16);
-
-    //
-    // Sign the PCR has with the AIK
+    // Sign the PCR hash with the AIK
     //
 
     QuoteResponse quote = tpm.Quote(
-        signingKey, Nonce, TPMS_NULL_SIG_SCHEME(), pcrsToQuote);
-
-    //
-    // Simulate verifying the quote at the server
-    //
-
-    TPMS_ATTEST qAttest = quote.quoted;
-    TPMS_QUOTE_INFO *qInfo = dynamic_cast<TPMS_QUOTE_INFO *> (qAttest.attested);
-    cout << "Quoted PCR: " << qInfo->pcrSelect[0].ToString() << endl;
-    cout << "PCR-value digest: " << qInfo->pcrDigest << endl;
-
-    ReadPublicResponse pubKey = tpm.ReadPublic(signingKey);
-    bool sigOk = pubKey.outPublic.ValidateQuote(pcrVals, Nonce, quote);
-
-    if (sigOk) {
-        cout << "The quote was verified correctly" << endl;
-    }
+        restrictedKey, decryptedSecret, TPMS_NULL_SIG_SCHEME(), pcrsToQuote);
 
     //
     // Create a user signing-only key in the storage hierarchy. 
@@ -275,63 +467,70 @@ void AttestationForIot()
         primaryKey,
         newSigningKey.outPrivate,
         newSigningKey.outPublic);
+    auto userSigningPubX = tpm.ReadPublic(keyToCertify);
+    TPMT_PUBLIC &userSigningPub = userSigningPubX.outPublic;
 
     //
     // Certify the creation of the user key using the AIK
     //
 
     CertifyCreationResponse createQuote = tpm.CertifyCreation(
-        signingKey,
+        restrictedKey,
         keyToCertify,
-        Nonce,
+        decryptedSecret,
         newSigningKey.creationHash,
         TPMS_NULL_SIG_SCHEME(),
         newSigningKey.creationTicket);
 
     //
-    // Simulate checking the key creation quote signature at the server
+    // Send the PCR quote and key certification to the server
     //
 
-    sigOk = pubKey.outPublic.ValidateCertifyCreation(
-        Nonce,
-        newSigningKey.creationHash,
+    ServerRegisterKey(
+        decryptedSecret,
+        restrictedPub,
+        pcrVals,
+        quote,
+        newSigningKey.creationData,
         createQuote);
-    if (sigOk) {
-        cout << "Key creation certification validated" << endl;
-    }
 
     //
     // Sign a message with the user key
     //
 
+    std::string clientMessage("some message or telemetry data");
     ByteVec messageHash = TPMT_HA::FromHashOfString(
-        TPM_ALG_ID::SHA1, "some message or telemetry data").digest;
+        TPM_ALG_ID::SHA1, clientMessage).digest;
     auto signature = tpm.Sign(
         keyToCertify,
         messageHash,
         TPMS_NULL_SIG_SCHEME(),
         TPMT_TK_HASHCHECK::NullTicket());
 
-    cout << "Signature with imported key: " << signature.ToString(false) << endl;
+    cout << "Client: message signature: " << signature.ToString(false) << endl;
 
     //
-    // Simulate checking the message signature at the server
+    // Send the signed message to the server
     //
 
-    ReadPublicResponse userPublic = tpm.ReadPublic(keyToCertify);
-    sigOk = userPublic.outPublic.ValidateSignature(
-        messageHash, *signature.signature);
-    if (sigOk) {
-        cout << "Message signature validated" << endl;
-    }
+    ServerReceiveMessage(
+        userSigningPub,
+        clientMessage,
+        *signature.signature);
+
+    //
+    // Save the message signing key to be reused until the PCR(s) change(s)
+    //
+
+    // TODO
     
     //
-    // Dump handles we're done with
+    // Close handles 
     //
 
     tpm.FlushContext(keyToCertify);
     tpm.FlushContext(primaryKey);
-    tpm.FlushContext(signingKey);
+    tpm.FlushContext(restrictedKey);
 }
 
 //
